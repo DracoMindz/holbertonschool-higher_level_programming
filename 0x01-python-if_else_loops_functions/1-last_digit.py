2d50e913ae7a03556bc6e2253a1023401194e85f
DracoMindz <704@holbertonschool.com>
2019-05-01 02:41:50 +0000

    Python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -17,5 +17,5 @@ if (lastnumber) == 0:
 if (lastnumber) > 5:
     print('is {} and is greater than 5'.format(lastnumber))
 
-if (lastnumber) < 6 and (number % 10) != 0:
+if (lastnumber) < 6 and (lastnumber) != 0:
     print('is {} and is less than 6 and not 0'.format(lastnumber))
