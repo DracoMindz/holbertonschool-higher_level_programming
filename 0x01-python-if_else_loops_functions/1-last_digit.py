1b406730a418698d44e10bce2653d5de8dcecf58
DracoMindz <704@holbertonschool.com>
2019-05-01 00:33:50 +0000

    Python positive, negative and zero

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,23 +3,17 @@ import random
 number = random.randint(-10000, 10000)
 
 
-def lastDigit(num):
+def last(number):
     return (number % 10)
 
-if lastDigit(num) == 0:
-    print(
-        'Last digit of {} is {}'
-        'and is zero'
-        .format(number, lastDigit(num))
+if last(number) == 0:
+    print("Last digit of {} is {}\
+          and is zero".format(number, last(number)))
 
-if lastDigit(num) > 5:
-    print(
-        'Last digit of {} is {} and'
-        'is greater than 5'
-        .format(number, lastDigit(num))
+if last(number) > 5:
+    print("Last digit of {} is {}\
+           and is greater than 5".format(number, last(number)))
 
-if lastDigit(num) < 6 and lastDigit(num) != 0:
-    print(
-        'Last digit of {} is {} and is less'
-        'than 6 and not 0'
-        .format(number, lastDigit(num))
+if last(number) < 6 and last(number) != 0:
+    print("Last digit of {} is {}\
+          and is less than 6 and not 0".format(number, last(number)))
