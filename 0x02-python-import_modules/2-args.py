f382a87519c0009601d202a1bb11563b4cb19f49
DracoMindz <704@holbertonschool.com>
2019-05-04 17:05:25 +0000

    Python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
+    if len(argv) == 0:
+        print(len(argv), 'arguments.')
+        exit()
+    if len(argv) == 1:
+        print(len(argv) - 1, 'argument.')
+    else:
+        print(len(argv) - 1, 'arguments:')
     for n, x in enumerate(argv[1:]):
-        if len(argv) == 0:
-            print(len(argv), 'arguments.')
-            exit()
-        if len(argv) == 1:
-            print(len(argv), 'argument.')
-        else:
-            print(len(argv), 'arguments:')
         print("{}: {}".format(n, x))
