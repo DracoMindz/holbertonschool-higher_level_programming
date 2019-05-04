69209508a7c16497b3de3faca749690ef796dff9
DracoMindz <704@holbertonschool.com>
2019-05-04 18:59:26 +0000

    Python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from sys import hidden_4.pyc
-x = dir(hidden_4.pyc)
-for n in x:
-    if n[0] != "_" and n[1] != "_":
-        print(n)
+if __name__ == "__main__":
+    import hidden_4
+    x = dir(hidden_4)
+    for n in x:
+        if n[0] != "_" and n[1] != "_":
+            print(n)
