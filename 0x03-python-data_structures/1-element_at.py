4d51c4fc551cb1b9ad84f8a218f76bc171a46967
DracoMindz <704@holbertonschool.com>
2019-05-08 00:46:54 +0000

    Python

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-
-while (my_list):
-    if idx < 0 or idx > len(my_list):
-        return None
-    return idx
+    if (idx < 0 or idx > len(my_list)):
+            return None
+    return my_list[idx]
