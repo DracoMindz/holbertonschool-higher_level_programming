b91352ba64f5ab13259302f6097535dbb778593d
DracoMindz <704@holbertonschool.com>
2019-05-08 18:45:54 +0000

    Python

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    mina = my_list[0]
+    for i in my_list:
+        if i > mina:
+            mina = i
+    return mina
