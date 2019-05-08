a7cb9c14b1e2afb74ba3c6781ea0c21aecb22cfb
DracoMindz <704@holbertonschool.com>
2019-05-08 03:47:03 +0000

    Python

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,11 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * is_palindrome - check to see if linked lists are palindromes
+ * @head: pointer to a pointer
+ *
+ * Return: 0 in false and 1 if true
+ */
+int is_palindrome(listint_t **head)
+{
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+
+    [[ row [i] for row in matrix] for i in range(4)]
