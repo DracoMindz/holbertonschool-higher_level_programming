9c6e7e8fc1ae2313c45f5bb053f9a3ff2408971f
DracoMindz <704@holbertonschool.com>
2019-05-09 01:02:52 +0000

    palidrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,3 +9,40 @@
  */
 int is_palindrome(listint_t **head)
 {
+	int len, count;
+	listint_t *nodePointer, *endPointer;
+
+	if (head == NULL || *head == NULL)
+	{
+		return (NULL);
+	}
+	endPointer = *head;
+
+	for (len = 0; endPointer != NULL; len++)
+	{
+		endPointer = endPointer->next;
+		count = len;
+	}
+	nodePointer = *head;
+	endPointer = *head;
+
+	while ((count/2) > 0)
+	{
+		if (nodePointer->n == endPointer->n)
+		{
+			nodePointer = nodePointer->next;
+			{
+				while (len > 0)
+				{
+					endPointer = endPionter->next;
+				}
+		else:
+			return (0);
+	        len--;
+			}
+		}
+	}
+		nodePointer = *head;
+		endpointer = *head;
+		return (1);
+}
