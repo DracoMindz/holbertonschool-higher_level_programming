4a91b181eb2a7f18ba0d9577b0833a99ec6d04ae
DracoMindz <704@holbertonschool.com>
2019-05-21 03:04:00 +0000

    Python

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         quot = a / b
     except:
-        quot =  None
+        quot = None
     finally:
         print("Inside result: {}".format(quot))
     return quot
