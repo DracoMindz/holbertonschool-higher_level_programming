e5e732fe74d6cd37f1aad15deb26e80ee15b7e0b
DracoMindz <704@holbertonschool.com>
2019-05-28 06:08:31 +0000

    Python

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
-"""
-Function will print first and last name.  Function takes in input.
+    """
+    Function will print first and last name.  Function takes in input.
 
-Args:
-first_name is input from user or main
-last_name is input from user of main
+    Args:
+    first_name is input from user or main
+    last_name is input from user of main
 
-Raises:
-TypeError("first_name must be a string")
-TypeError("last_name must be a string")
+    Raises:
+    TypeError("first_name must be a string")
+    TypeError("last_name must be a string")
 
-Return:
-("My name is {:s} {:s}".format(first_name, las\|
-  t_name))
-"""
-        if  not isinstance(first_name, str):
-            raise TypeError("first_name must be a string")
-        if not isinstance(last_name, str):
-            raise TypeError("last_name must be a string")
-        else:
-            print("My name is {:s} {:s}".format(first_name, last_name))
+    Return:
+    ("My name is {:s} {:s}".format(first_name, las\|
+    t_name))
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {:s} {:s}".format(first_name, last_name))
