02194bcd4fb2ddeab2e395546773587484f307ff
DracoMindz <704@holbertonschool.com>
2019-05-28 04:50:54 +0000

    Python

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-"""
-Function adds integers
+    """
+    Function adds integers
 
-Args:
-a and b are integers
+    Args:
+    a and b are integers
 
-Raises:
-TypeError("a must be an integer")
-TypeError("b must be an integer")
+    Raises:
+    TypeError("a must be an integer")
+    TypeError("b must be an integer")
 
-Return: Sum
-"""
-
-    sum  = int(a) + int(b)
-
-    if not isinstance(a, int) or  not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, int) or not isinstance(b, float):
-        raise TypeError("b must be an integer")
-    else:
-        return(sum)
+    Return: Sum
+    """
+    if not isinstance(a, (int, float)):
+        raise ValueError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise ValueError('b must be an integer')
+    sum = int(a) + int(b)
+    return(sum)
