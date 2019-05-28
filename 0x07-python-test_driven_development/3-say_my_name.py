d5b4e5a9324a9849185f6067c87e49063ce22917
DracoMindz <704@holbertonschool.com>
2019-05-28 13:45:29 +0000

    Python

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,6 +15,7 @@ def say_my_name(first_name, last_name=""):
     ("My name is {:s} {:s}".format(first_name, las\|
     t_name))
     """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
