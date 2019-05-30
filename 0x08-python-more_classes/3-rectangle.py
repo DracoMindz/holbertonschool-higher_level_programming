ff546a7acd7529f363ce00116c05470b433452eb
DracoMindz <704@holbertonschool.com>
2019-05-30 18:02:14 +0000

    Python

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -41,12 +41,10 @@ class Rectangle:
         self.__height = value
 
     def __str__(self):
-        if (self.width == 0 or self.height == 0):
+        new_string = []
+        if (self.__width is 0 or self.__height is 0):
             return ""
         else:
-            for j in range(self.height):
-                return ""
-            for i in range(self.width):
-                self.string = (" " *self.height)
-                self.strang = ("#" * self.width)
-                return(" " * self.height + "#" * self.width)
+            for j in range(self.__height):
+                new_string.append("#" * self.__width)
+            return '\n'.join(new_string)
