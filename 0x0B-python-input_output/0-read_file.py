26c59bf958d4299e61b1a424a1c4df9bfa899f5a
DracoMindz <704@holbertonschool.com>
2019-06-06 23:41:57 +0000

    Python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     with open(filename, 'r', encoding= "utf-8") as f:
-        print(f.read())
+        read_data = f.read()
+        print(read_data, end="")
