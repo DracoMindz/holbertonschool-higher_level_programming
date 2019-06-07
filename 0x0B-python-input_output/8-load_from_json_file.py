0af4ca469f9bc69de78ba47ad33c7ffb3bc91e02
DracoMindz <704@holbertonschool.com>
2019-06-07 01:41:27 +0000

    Python

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+"""
+json object pass
+"""
+import json
+
+
 def load_from_json_file(filename):
+    """
+    function to load json object
+    """
+    with open(filename) as f:
+        return json.load(f)
