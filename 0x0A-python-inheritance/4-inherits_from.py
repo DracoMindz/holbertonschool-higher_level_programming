0ff17eb4c300da774cdd69fc1a6342ea788b8e27
DracoMindz <704@holbertonschool.com>
2019-06-07 17:44:30 +0000

    Python

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
-    if type(obj) is not a_class:
+    if type(obj) is not a_class and isinstance(obj, a-class):
         return True
     else:
         return False
