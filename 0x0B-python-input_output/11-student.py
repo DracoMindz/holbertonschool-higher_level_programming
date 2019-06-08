003b5bc7e9baa55788d7d4dcd99a0bdce68fbeb9
DracoMindz <704@holbertonschool.com>
2019-06-08 19:48:33 +0000

    Python

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""
+Student class
+"""
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.fn = first_name
+        self.ln = last_name
+        self.age = age
--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python3
 import json
+import sys
 
 
-def class_to_json(obj):
-    
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w') as filename:
+        json.dump(my_obj, filename)
+
+
+def load_from_json_file(filename):
+    """
+    function to load json object
+    """
+    with open(filename) as f:
+        return json.load(f)
+
+
+def add_item(args):
+    """
+    function to add all arguemts to a Python List and save to json file
+    """
+
+    myList = []
+    myListList = mylist.extend(args)
+    save_to_json_file(myListList, add_item.js)
+    load_from_json_file(add_item.js)
