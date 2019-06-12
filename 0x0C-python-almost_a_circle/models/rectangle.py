1ea9934cfb028945b9db0cbea001cd06f6a0203a
DracoMindz <704@holbertonschool.com>
2019-06-12 19:21:05 +0000

    Python Not Circles

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,76 +12,72 @@ class Rectangle(Base):
         Private instance attributes, with their own public getter/setter.
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-    @property #method to get value/properties of private width
+    @property   # method to get value/properties of private width
     def width(self):
         return self.__width
 
-    @width.setter #setting value of width
+    @width.setter  # setting value of width
     def width(self, value):
 
         """
         Taske: TypeError for non integer, ValueError for negative numbers
         """
-        if isinstance(value, int):
-            self.__width = value
-        else:
+        if not type(value):
             raise TypeError('width must be an integer')
         if (value < 0):
             raise ValueError('width must be > 0')
+        self.__width = value
 
-    @property #method to get value/properties of private height
+    @property  # method to get value/properties of private height
     def height(self):
         return self.__height
 
-    @height.setter #setting value of height
+    @height.setter   # setting value of height
     def height(self, value):
 
         """
         Task 3 TypError for non integer, ValueError for negative numbers
         """
-        if isinstance(value, int):
-            self.__height = value
-        else:
+        if not type(value):
             raise TypeError('height must be an integer')
         if (value < 0):
             raise ValueError('height must be > 0')
+        self.__height = value
 
-    @property #method to get valyes/properties of private x
+    @property   # method to get valyes/properties of private x
     def x(self):
         return self.__x
 
-    @x.setter #setting value of x
+    @x.setter   # setting value of x
     def x(self, value):
 
         """
         Task 3 TypeError for non integer, ValueError for negative numbers
         """
-        if isinstance(value, int):
+        if type(value):
             self.__x = value
         else:
             raise TypeError('x must be an integer')
         if (value < 0):
             raise ValueError('x must be >= 0')
 
-    @property #methos to get value/properties of private y
+    @property   # methos to get value/properties of private y
     def y(self):
         return self.__y
 
-    @y.setter #setting value of y
+    @y.setter   # setting value of y
     def y(self, value):
 
         """
         Task 3 TypeError for non integer. ValueError for negative numbers
         """
-        if isinstance(value, int):
-            self.__y = value
-        else:
+        if not type(value):
             raise TypeError('y must be an integer')
         if (value < 0):
             raise ValueError('y must be >= 0')
-        print(self.__y)
+        self.__y = value
