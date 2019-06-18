02c7be5c4bebccb1565ac3ef539f77296adf913a
DracoMindz <veganengineering@gmail.com>
2019-06-18 22:24:51 +0000

    Pyhton

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,29 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 if__name__ == "__main__":
 from sys import argv
 from calculator_1.py import add, sub, mul, div
 
-          if len(argv) not 3:
-              print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
-              exit(1)
+     if len(argv) not 3:
+         print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+         exit(1)
           
-          operator = argv[2]
-          a = int(argv[1])
-          b = int(argv[3])
+     operator = argv[2]
+     a = int(argv[1])
+     b = int(argv[3])
 
-          if operator not "+" or "-" or "/" or "*":
-              print("Unknown operator. Available operators: +, -, * and /\n")
-              exit(1)
-          if operatotor is "+":
-              a + b = result
-              print("{:d} + {:d} = {:d}".format(a, operator, b, result)
-          if operator is "-":
-              a - b = result
-              print("{:d} - {:d} = {:d}".format(a, operator, b, result)
-          if operator is "*":
-              a * b = result
-              print("{:d} * {:d} = {:d}".format(a, operator, b, result)
-          if operator is "/":
-              a / b = result
-              print("{:d} / {:d} = {:d}".format(a, operator, b, result)
+     if operator not "+" or "-" or "/" or "*":
+         print("Unknown operator. Available operators: +, -, * and /\n")
+         exit(1)
+     if operatotor is "+":
+         a + b = result
+         print("{:d} + {:d} = {:d}".format(a, operator, b, result)
+     if operator is "-":
+         a - b = result
+         print("{:d} - {:d} = {:d}".format(a, operator, b, result)
+     if operator is "*":
+         a * b = result
+         print("{:d} * {:d} = {:d}".format(a, operator, b, result)
+     if operator is "/":
+         a / b = result
+         print("{:d} / {:d} = {:d}".format(a, operator, b, result)
     
