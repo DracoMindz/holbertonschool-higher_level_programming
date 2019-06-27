0dff8037b593cf3d2559a98b9336783d6113e3f1
DracoMindz <veganengineering@gmail.com>
2019-06-27 21:21:33 +0000

    Python

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,6 +4,9 @@ Student class
 """
 class Student:
     def __init__(self, first_name, last_name, age):
-        self.fn = first_name
-        self.ln = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
+
+    def to_json(self):
+        return self.__dict__
