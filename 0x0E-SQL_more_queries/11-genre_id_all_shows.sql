cd96c87f041d1ff04dcb15eeef96c4ee90ecd83d
DracoMindz <DracoMindz@github.com>
2019-07-17 20:09:42 +0000

    join task 13

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,3 @@
 -- Import the database dump of hbtn_0d_tvshows to your MySQL server: download 
 -- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
-SELECT name AS genre, COUNT (*) AS number_of_shows FROM tv_genres GROUP BY genre FROM tv_show_genre JOIN ON genre.id = id GROUP BY genre ORDER BY number_of_shows DESC
+SELECT name AS genre, COUNT (*) AS number_of_shows FROM tv_genres GROUP BY genre FROM tv_show_genre JOIN ON genre_id = id GROUP BY genre ORDER BY number_of_shows DESC;
