b8490edfe6561e992fac2b3bb91074883fe432f3
DracoMindz <DracoMindz@github.com>
2019-07-17 01:46:15 +0000

    Table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,2 +1,4 @@
 -- script that creates the database hbtn_0d_usa and the table states
 -- if the database or the table already existy script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id) ); 
