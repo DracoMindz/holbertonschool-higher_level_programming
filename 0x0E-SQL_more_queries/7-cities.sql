d611d19d25e20db09a23b3bfa9be8c6acc0d9182
DracoMindz <DracoMindz@github.com>
2019-07-17 02:16:16 +0000

    adding another table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,8 @@
 -- adding state id to the columns from task 6
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-       id INT UNIQUE NOT NULL AUTO_INCREMENT, 
+       id INT UNIQUE AUTO_INCREMENT NOT NULL, 
        state_id INT NOT NULL ,
        name VARCHAR(256) NOT NULL,
        PRIMARY KEY (id),
-       FOREIGN KEY (state_id) 
-       	       REFERENCES states(id)); 
+       FOREIGN KEY (state_id) REFERENCES states(id)); 
