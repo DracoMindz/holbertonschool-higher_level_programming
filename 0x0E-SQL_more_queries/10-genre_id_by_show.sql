4687c8a4877fdb9a811902a89ffe3a9378bd9cf8
DracoMindz <DracoMindz@github.com>
2019-07-17 03:56:41 +0000

    mysql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
+-- Write a script that lists all shows contained in database that have at least one genre linked.
+mysql -u root -p < hbtn_0d_tvshows.sql;
+SELECT tv_shows.title, tv_show_genres.genre_id, FROM hbtn_0d_tvshow WHERE qty tv_show_genres = 1 GROUP BY tv_shows.title, tv_show_genres.genre_id ASC;
