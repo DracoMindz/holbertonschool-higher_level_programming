4bc047aa829c71658e9f1f81a672225b0da24d5c
DracoMindz <DracoMindz@github.com>
2019-07-17 21:30:17 +0000

    sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,4 @@
--- Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+--  script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
 -- The database name will be passed as an argument of the mysql command
-
-mysql -u root -p < hbtn_0d_tvshows.sql;
-SELECT tv_shows.title, tv_show_genres.genre_id, FROM hbtn_0d_tvshow WHERE qty tv_show_genres = 1 GROUP BY tv_sho\
-ws.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genre_id FROM tv_shows LEFT JOIN tv_show_genres ON id = show_id WHERE genre.id = NULL ORDER BY title ASC, genre_id ASC;
+ 
