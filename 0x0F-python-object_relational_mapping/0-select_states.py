95a1c62b7366117e31e48579e7c3417865e18d71
DracoMindz <DracoMindz@github.com>
2019-08-10 23:14:42 +0000

    States data

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ conn = MySQLdb.connect(host="localhost", port=3306,
                        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 cur = conn.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
-# HERE I have to know SQL to grab all states in my database
+#grab all states in my database
 query_rows = cur.fetchall()
 for row in query_rows:
     print(row)
