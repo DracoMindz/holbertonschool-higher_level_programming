94f4f18e5f17c5183daca72645ad1b5aae65b1ee
DracoMindz <DracoMindz@github.com>
2019-08-11 06:07:55 +0000

    Mapping fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,14 +2,18 @@
 # Using States Data
 import MySQLdb
 import sys
-from sys import argv
 
-conn = MySQLdb.connect(host="localhost", port=3306,
-                       user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == '__main__':
+conn = MySQLdb.connect(
+       host="localhost",
+       port=3306,
+       user=sys.argv[1],
+       passwd=sys.argv[2],
+       db=sys.argv[3])
+       cur = conn.cursor()
+       cur.execute("SELECT * FROM states ORDER BY id ASC")
+       query_rows = cur.fetchall()
+       for row in query_rows:
+           print(row)
+       cur.close()
+       conn.close()
