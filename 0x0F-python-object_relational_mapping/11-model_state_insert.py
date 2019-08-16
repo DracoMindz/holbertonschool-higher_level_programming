59d9f31f4bd25a9a5b0519e8cdc6c4f0b59d7bb4
DracoMindz <DracoMindz@github.com>
2019-08-16 18:42:35 +0000

    use filter by name

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# add a new state
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    entry = State(name='Louisiana')
+    session.add(entry)
+    entryfirst = session.query(State).filter_by(name='Louisiana').first()
+    print(entryfirst.id)
+    session.commit
+    session.close()
