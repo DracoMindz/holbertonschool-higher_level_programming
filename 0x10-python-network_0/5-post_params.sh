1ab9b67f5c6a23053312371331da70a37405e51b
DracoMindz <DracoMindz@github.com>
2019-08-21 02:09:46 +0000

    using POST with email= and subject=

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script send a POST request passed to URL; variables: email, subject
-curl -X POST -s -d "email=hr@holbertonschool.com&subject=I will always be here for PLD""$1"
+curl -X POST -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
