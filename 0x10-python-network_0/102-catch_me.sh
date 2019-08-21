5d9f0f24513b47c6d416395917c37653abc6a6c0
DracoMindz <DracoMindz@github.com>
2019-08-21 03:31:51 +0000

    Catch me You got me (clues)

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Request to catch_me server response You got me!"
+#Request to catch_me server response You got me!"
 curl -X PUT -sLd "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
