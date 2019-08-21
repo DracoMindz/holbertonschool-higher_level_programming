8a1dd558cab291266d2e201d00cbdf71c4113470
DracoMindz <DracoMindz@github.com>
2019-08-21 02:51:49 +0000

    Json POST request with 2 args

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script sends a JSON POST, request URL as argument,
-curl -X POST -H "Content-Type: application/json" -d "@$2" "$1"
+curl -X POST -sH "Content-Type: application/json" -d "@$2" "$1"
