1961031030df1ce149c9f3c7a766e455deb97906
DracoMindz <DracoMindz@github.com>
2019-08-21 01:06:20 +0000

    curL only 200 status code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # takes a URL, sends a GET request, displays body of response
-
-if [ $'(curl -L -s -X HEAD  -w "%{http_code}""$1")' == '200' ]
-then
-    curl -Ls "$1"
-fi
+curl -sL "$1"
