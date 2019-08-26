262087226bf9721b78c175f559b4fa144bd61651
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-08-26 18:18:37 -0400

    URL request and print response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 #write a python script that fetches a url
-import urllib
-from urllib import request
+import urllib.request
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
     print("Body response:")
     print("\t - type: {}".format(type(html)))
-    print("\t - content:{}".format(html))
+    print("\t - content: {}".format(html))
     print("\t - utf8 content: {}".format(html.decode('utf8')))
