80c1b96ad81e555ef32f6c8c7e708fdfc89f0360
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-08-26 18:30:04 -0400

    Url request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-#write a python script that fetches a url
 import urllib.request
-
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
