96cd6c4dcfccefe499d0d18b4e88026d4cf04112
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-08-26 23:22:54 -0400

    used argv as variable

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-"""
-Script that takes in a URL, sends a request to the URL
-Displays the value of the X-Request-Id variable
-from the header of the response.
-"""
 import urllib.request
 import sys
-
-
 if __name__ == "__main__":
-    arg_url = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(arg_url) as response:
+    sys.argv[1] = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.info().get('X-Request-Id'))
