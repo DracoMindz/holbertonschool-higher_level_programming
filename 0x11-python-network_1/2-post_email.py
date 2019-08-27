68e7363abdff2135d887deddad4b8180cf7ed2d4
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-08-27 01:24:37 -0400

    Changed the variable data

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
     dat = urllib.parse.urlencode(values)
-    dat = data.encode('ascii')
+    dat = dat.encode('ascii')
     request = urllib.request.Request(url, dat)
     with urllib.request.urlopen(request) as response:
         print(response.decode().decode('utf-8'))
