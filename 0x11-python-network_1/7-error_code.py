26817297ada0393b18249b9e68300849f8367479
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-08-27 15:51:40 -0400

    Python NetworK: include raise_for_status in if statement

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Script takes url, send req to url, diplay body response"""
+""" Script takes url, send req to url, diplay body response """
 import requests
 import sys
 
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    if r.status_code == r.raise_for_status():
+    if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
