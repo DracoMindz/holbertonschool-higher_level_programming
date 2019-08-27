7d4f25ba01e8052eb940c2c7e15321bbf65d8820
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-08-27 13:57:58 -0400

    Update syntax: space near Body response:

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" script fetches url, use request package"""
+""" script fetches url, use request package """
 import requests
 
 if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status').text
-    print("Body response: ")
+    print("Body response:")
     print("\t- type: {}".format(type(r)))
     print("\t- content: {}".format(r))
