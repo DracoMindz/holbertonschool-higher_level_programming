fc9f41dd3e9bf90cf86f5c33ccd0e919eb3a42d5
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-08-27 01:22:36 -0400

    renamed variable data

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,8 +10,8 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(values)
-    data_e = data.encode('ascii')
-    request = urllib.request.Request(url, data_e)
+    dat = urllib.parse.urlencode(values)
+    dat = data.encode('ascii')
+    request = urllib.request.Request(url, dat)
     with urllib.request.urlopen(request) as response:
         print(response.decode().decode('utf-8'))
