67bd71a57934c8ce262013b6d3d99456adbe81b7
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-08-27 12:56:13 -0400

    changed libraries

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,16 +2,14 @@
 """
 script handles error codes
 """
-import urllib.request
 import sys
-from urllib.request import Request, urlopen
-from urllib.error import HTTPError
-
-req = Request(sys.argv[1])
+import urllib.request
+import urllib.error
+req = urllib.request.Request(sys.argv[1])
 
 if __name__ == "__main__":
     try:
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
-    except HTTPError as e:
-        print('Error code: ', e.code)
+    except urllib.error.HTTPError as error:
+        print("Error code: {}".format(error.code))
