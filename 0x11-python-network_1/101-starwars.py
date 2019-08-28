5d58fd11d127fc36b667d3b9eaa52ffaa7e220b6
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-08-27 20:02:44 -0400

    Python Network #1: update 9 with pagination

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Python script takes string, sends request, manage pagination (json) """
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://swapi.co/api/people/?search={}'.format(sys.argv[1])
+    r = requests.get(url)
+    print('Number of results: {}'.format(r.json()['count']))
+    while (r):
+        for bob in r.json()['results']:
+            print(bob['name'])
+        try:
+        r_next = requests.get(r.json()['next'])
+        except:
+            break
