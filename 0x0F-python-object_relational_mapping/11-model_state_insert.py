718f4780aae23422c7b95075b003359a0a3914be
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-07 13:07:05 -0400

    Update to the args

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-# add a new state
+# add a new state object Louisiana
 
 
-from sys import argv
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3]))
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    # the function takes in thre arguments
 
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
+
     entry = State(name='Louisiana')
     session.add(entry)
-    entryfirst = session.query(State).filter(name == 'Louisiana').first()
-    print(entryfirst.id)
-    session.commit
-#end
-    
+    session.commit()
+
+    print("{}".format(entry.id))
