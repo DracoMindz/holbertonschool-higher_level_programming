669a242aa5cfac7d4460a4454ace782e0d5b0f0c
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-07 18:32:13 -0400

    Update: Include Query adn delete

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# Change: delete all State objects with an a in the name
+# script connects via sqlachemy to mysql db local host port 3306
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, update
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    # the function takes in three arguments
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # create variable for query search
+    # filter out the states that contain and 'a'
+    states_with_a = session.query(State).filter(State.name.contains('a')
+    # if a state meets the definition of state_with_a delete it
+    for state in states_with_a:
+        session.delete(state)
+
+    session.commit()
