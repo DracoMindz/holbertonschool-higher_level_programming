91cef94436c5d61b15f1b02f60a8e25835367eb2
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-09 11:39:49 -0400

    Update: session  query

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# Change: delete all State objects with an a in the name
-# script connects via sqlachemy to mysql db local host port 3306
-
+"""
+Change: delete all State objects with an a in the name
+script connects via sqlachemy to mysql db local host port 3306
+"""
 
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine, update
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
@@ -20,9 +21,7 @@ if __name__ == "__main__":
 
     # create variable for query search
     # filter out the states that contain and 'a'
-    states_with_a = session.query(State).filter(State.name.contains('a')
-    # state meets the definition of state_with_a delete it
-    for state in states_with_a:
-        session.delete(state)
+    session.query(State).filter(State.name.contains('a')) \
+        .delete(state)
 
-    session.commit()
+    session.commit
