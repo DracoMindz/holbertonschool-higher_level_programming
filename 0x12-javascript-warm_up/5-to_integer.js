2655feced788ba16c7b205b8190e8de4721abba4
DracoMindz <DracoMindz@github.com>
2019-09-18 03:57:15 +0000

    parsed

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// convert arguemtn to integer
+
+const parsed = parseInt(process.argv[2]);
+
+if (isNAN(parced)) { console.log('Not a number')
+} else {
+    console.log('My Number: ' + parced);
+}
