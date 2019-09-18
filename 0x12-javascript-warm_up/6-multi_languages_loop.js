17f0c37f3a13aa232f21abaea7f95565b96412f6
DracoMindz <DracoMindz@github.com>
2019-09-18 11:00:40 +0000

    for loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 // three line array
-const myArray = ['c is fun', 'Pyhton is cool', 'Javascript is amazing'];
+const myArray = ['C is fun', 'Pyhton is cool', 'Javascript is amazing'];
 for (let i = 0; i < myArray.length; i++) {
   console.log(myArray[i]);
 }
