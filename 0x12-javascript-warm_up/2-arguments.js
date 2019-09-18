8bf115c69e694c74ec5ef440dced4d773ecdb234
DracoMindz <DracoMindz@github.com>
2019-09-18 03:01:57 +0000

    Process.argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// print process.argv
+if (process.argv.slice(2).length === 0) {
+  console.log('No argument');
+} else if (process.argv.slice(2).length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
