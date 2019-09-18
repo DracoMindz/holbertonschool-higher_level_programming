95edac1e4364b0c8f550112d945b2032904530c8
DracoMindz <DracoMindz@github.com>
2019-09-18 05:52:43 +0000

    Syntax

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-// convert arguement to integer
-
 const p = parseInt(process.argv[2]);
 
 if (isNaN(p)) {
   console.log('Not a number');
 } else {
-  console.log('My Number: ' + p);
+  console.log('My number: ' + p);
 }
