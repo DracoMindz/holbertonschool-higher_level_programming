5e681e4ffe789d20f6c018086a1a20ecc7955df3
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-24 00:08:25 -0400

    Update: charPrint

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,12 @@
 #!/usr/bin/node
-const Square = require('./5-square.js');
-class Square extends Rectangle {
-  contructor (size) {
-    super(size, size);
-	}
-
+class Square extends require('./5-square.js') {
 	charPrint(c) {
 		if (c === undefined) {
-			this.print();
+			super.print();
 		} else {
 			for (let m = 0; m < this.height; m++) {
 				console.log(c.repeat(this.width));
 			}
 	 }
-	
-
 }
 module.exports = Square;
