78ff4dfe99cf345c58d1b5a3d9479bfc713e183f
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-23 22:59:05 -0400

    Update : change values

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+    constructor(w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    print() {
+        for (let m = 0; < this.height; m++) {
+            console.log('X'.repeat(this.width));
+        }
+    }
+
+    rotate() {
+        let box = this.width
+        this.width = this.height;
+        this.height = box;
+    }
+};
