604bbc2e617ee0e9fc7c7ba506ce2d8203003ddd
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-23 22:23:50 -0400

    Update: console

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,15 +3,14 @@
 module.exports = class Rectangle {
     constructor(w, h) {
         if (w > 0 && h > 0) {
-
             this.width = w;
             this.height = h;
         }
     }
 
     print() {
-        for (let i = 0; i < this.height, i++) {
+        for (let m = 0; < this.height; m++) {
             console.log('X'.repeat(this.width));
         }
     }
-}
+};
