22053fbbb267473c883c696d69a6e234ab447d00
DracoMindz <DracoMindz@github.com>
2019-09-24 12:19:57 +0000

    Update semistandard

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,28 +1,28 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print() {
-        for (let m = 0; m < this.height; m++) {
-            console.log('X'.repeat(this.width));
-        }
+  print () {
+    for (let m = 0; m < this.height; m++) {
+      console.log('X'.repeat(this.width));
     }
+  }
 
-    rotate() {
-        const tbox = this.width;
-        this.width = this.height;
-        this.height = tbox;
-    }
+  rotate () {
+    const tbox = this.width;
+    this.width = this.height;
+    this.height = tbox;
+  }
 
-    double() {
-        this.width *= 2;
-        this.height *= 2;
-    }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 module.exports = Rectangle;
