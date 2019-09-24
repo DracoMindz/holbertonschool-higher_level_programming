590d57bf2fc46b63e407443d9bc529bcb9db1eb5
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-24 00:15:21 -0400

    Update: for loop

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,10 @@
 class Square extends require('./5-square.js') {
 	charPrint(c) {
 		if (c === undefined) {
-			this.print();
-		} else {
-			for (let m = 0; m < this.height; m++) {
-				console.log(c.repeat(this.width));
+			c = 'X';
+		}
+		for (let m = 0; m < this.height; m++) {
+			 console.log(c.repeat(this.width));
 			}
 	 }
 }
