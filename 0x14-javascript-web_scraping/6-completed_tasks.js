44ad818eae2ab36c9e5359520b77ca2ec8679751
DracoMindz <DracoMindz@github.com>
2019-09-29 16:24:12 +0000

    Include map

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log(error);
-  }
-  const bobj = {};
-  const binfo = JSON.parse(body);
-  for (const num of binfo) {
-    if (num.completed === true) {
+    if (error) {
+	console.log(error);
+    }
+    const bobj = {};
+    const binfo = JSON.parse(body);
+    for (const num of binfo) {
+	if (num.completed === true) {
 	    if (bobj[num.userId] === undefined) {
-        bobj[num.userId] = 0;
+		bobj[num.userId] = 0;
 	    }
 	    bobj[num.userId]++;
+	}
     }
-  }
-  console.log(bobj);
+    console.log(bobjg);
 });
