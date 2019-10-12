de07e56417197bc82d98bf0cfad35d2d9f9caa43
DracoMindz <DracoMindz@github.com>
2019-10-12 22:39:07 +0000

    update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 const movurl = 'https://swapi.co/api/films' + process.argv[2];
-
-request.get(movurl, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
+request.get(movurl, (err, response, body) => {
+  if (err) throw err;
+  else {
     const characters = JSON.parse(response.body).characters;
     for (let index = 0; index < characters.length; index++) {
-      request.get(characters[index], (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
+      request.get(characters[index], (err, response, body) => {
+        if (err) throw err;
+        else {
           console.log(JSON.parse(response.body).name);
         }
       });
